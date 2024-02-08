33bb0919d242197451448bd42a29d48c12c0761f
HelplessSoldier <patrick1234143211@gmail.com>
2024-02-08 12:21:38 -0800

    done desktop, starting on mobile

--- 01_urbanNest_interiors/01_urbanNest/src/sections/footer/Footer.jsx
+++ 01_urbanNest_interiors/01_urbanNest/src/sections/footer/Footer.jsx
@@ -1,5 +1,25 @@
 import "./Footer.css";
 
 export default function Footer() {
-  return <h1>Hi from the footer comp!</h1>;
+  return (
+    <div className="footerRoot">
+      <div className="footerTextContainer">
+        <p>
+          Connect with Us:
+          <br /> Follow us on social media for design inspiration, tips, and
+          updates.
+        </p>
+        <p>
+          Stay in the loop:
+          <br /> Subscribe to our newsletter for the latest design trends and
+          exclusive offers.
+        </p>
+        <p>
+          Contact Us:
+          <br /> Have a question or want to schedule a consultation? Reach out to
+          our team today.
+        </p>
+      </div>
+    </div>
+  );
 }

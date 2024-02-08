eb7b9f6d35b4ac04880890ae6f2ef0b58180195f
HelplessSoldier <patrick1234143211@gmail.com>
2024-02-07 17:35:35 -0800

    eod ;

--- 01_urbanNest_interiors/01_urbanNest/src/sections/contact/Contact.jsx
+++ 01_urbanNest_interiors/01_urbanNest/src/sections/contact/Contact.jsx
@@ -1,5 +1,16 @@
 import "./Contact.css";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
-  return <h1>hi from the contect comp!</h1>
+  return (
+    <div className="contactRoot">
+      <h1>
+        Ready to take the first step towards transforming your space with
+        UrbanNest Interiors? We're here to turn your design dreams into reality.
+        Contact us today to schedule a consultation or inquire about our
+        services.
+      </h1>
+      <ContactForm />
+    </div>
+  );
 }
---
+++ 01_urbanNest_interiors/01_urbanNest/src/sections/contact/ContactForm.jsx
@@ -0,0 +1,10 @@
+export default function ContactForm() {
+  return (
+    <div className="contactFormRoot">
+      <div className="contactLabelInputPair">
+        <label htmlFor="" ></label>
+        <input />
+      </div>
+    </div>
+  )
+}

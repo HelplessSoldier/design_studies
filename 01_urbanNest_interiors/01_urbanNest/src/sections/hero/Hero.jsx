9c68a66378e3f0310673fcca7ee25e2d33eb1721
HelplessSoldier <patrick1234143211@gmail.com>
2024-02-07 16:42:31 -0800

    hero

--- 01_urbanNest_interiors/01_urbanNest/src/sections/hero/Hero.jsx
+++ 01_urbanNest_interiors/01_urbanNest/src/sections/hero/Hero.jsx
@@ -1,5 +1,22 @@
-import './Hero.css';
+import "./Hero.css";
 
 export default function Hero() {
-  return <h1>hi from the hero comp!</h1>
+  return (
+    <div className="heroContainer">
+      <div className="heroTextContainer">
+        <div className="heroText">
+          <h1>
+            UrbanNest Interiors is committed to eco-friendly and sustainable
+            design practices.
+          </h1>
+          <h2>
+            We believe in innovative and unique design solutions that reflect the
+            individuality of each client and space. While building strong
+            connections within the community and supporting local artisans.
+          </h2>
+        </div>
+      </div>
+      <img className="heroImage" src="./images/portfolio_02_raw.jpg" />
+    </div>
+  );
 }
